ffb863dd4512e24208707bd9c56bc62ac7bbea7f
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-10-23 09:34:22 +0530

    Update

--- PythonCall/sniff.py
+++ PythonCall/sniff.py
@@ -1,5 +1,5 @@
 import requests
-r = requests.post("http://localhost:9000/sniff/bandwidth", data={'bandwidth': 135})
+r = requests.post("http://localhost:9000/sniff/bandwidth", data={'bandwidth': 230})
 print r.status_code, r.reason
 contents = requests.get("http://localhost:9000/sniff/bandwidth")
 print contents
